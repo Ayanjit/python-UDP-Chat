0b35b6f24ff29fca3002f32ba36777437990d46b
Ayanjit <arora.sten@gmail.com>
2018-05-22 23:00:34 +0530

    Add files via upload

---
+++ Chat.py
@@ -0,0 +1,85 @@
+import socket
+import threading
+import queue
+import sys
+import random
+
+#Client Code
+def ReceiveData(sock,stop):
+    while not stop:
+        try:
+            data,addr = sock.recvfrom(1024)
+            print(data.decode('utf-8'))
+        except:
+            pass
+
+def RunClient(serverIP):
+    host = socket.gethostbyname(socket.gethostname())
+    port = random.randint(6000,10000)
+    print('Client IP->'+str(host)+' Port->'+str(port))
+    server = (str(serverIP),5000)
+    stop = False
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    s.bind((host,port))
+
+    name = input('Please write your name here: ')
+    s.sendto(name.encode('utf-8'),server)
+    threading.Thread(target=ReceiveData,args=(s,stop)).start()
+    while True:
+        data = input()
+        if data == 'qqq':
+            stop = True
+            break
+        elif data=='':
+            continue
+        data = '['+name+']' + '->'+ data
+        s.sendto(data.encode('utf-8'),server)
+    s.close()
+#Client Code Ends Here
+
+
+#Server Code
+def RecvData(sock,recvPackets):
+    while True:
+        data,addr = sock.recvfrom(1024)
+        recvPackets.put((data,addr))
+
+def RunServer():
+    host = socket.gethostbyname(socket.gethostname())
+    port = 5000
+    print('Server hosting on IP-> '+str(host))
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    s.bind((host,port))
+    clients = set()
+    recvPackets = queue.Queue()
+
+    print('Server Running...')
+
+    threading.Thread(target=RecvData,args=(s,recvPackets)).start()
+
+    while True:
+        while not recvPackets.empty():
+            data,addr = recvPackets.get()
+            if addr not in clients:
+                clients.add(addr)
+                continue
+            clients.add(addr)
+            data = data.decode('utf-8')
+            print(str(addr)+data)
+            for c in clients:
+                if c!=addr:
+                    s.sendto(data.encode('utf-8'),c)
+    s.close()
+#Serevr Code Ends Here
+
+if __name__ == '__main__':
+    if len(sys.argv)==1:
+        RunServer()
+    elif len(sys.argv)==2:
+        RunClient(sys.argv[1])
+    else:
+        print('Run Serevr:-> python Chat.py')
+        print('Run Client:-> python Chat.py <ServerIP>')
+
+
+    
